0d0dd745235cce078d248b49389b16048f11836a
Andrew Henry <ajhenry@github.com>
2024-02-22 13:26:58 -0500

    fix: remove setup tests for jest

--- test/setupJest.ts
+++
@@ -1,8 +0,0 @@
-import AbortController from 'abort-controller'
-import fetch from 'node-fetch'
-import ws from 'ws'
-
-const globalAny = global as any
-globalAny.AbortController = AbortController
-globalAny.fetch = fetch
-globalAny.WebSocket = ws
