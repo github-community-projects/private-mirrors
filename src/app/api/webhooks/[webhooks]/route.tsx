49c08eebdb6656dfcaed5d912f234d927c82d1b4
Jacob Sutter <sutterj@github.com>
2024-05-01 19:16:37 -0400

    fix: build error with config
    - remove config as bodyParser is not needed with the app router

--- src/app/api/webhooks/[webhooks]/route.tsx
+++ src/app/api/webhooks/[webhooks]/route.tsx
@@ -1,16 +1,8 @@
-import app from '../../../../bot'
 import { createNodeMiddleware, createProbot, Logger } from 'probot'
+import app from '../../../../bot'
 import { logger } from '../../../../utils/logger'
 
 export const probot = createProbot()
-
-// Disable the bodyParser to allow the raw body to be read
-// https://github.com/github-community-projects/internal-contribution-forks/issues/88
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
 
 export default createNodeMiddleware(app, {
   probot: createProbot({
