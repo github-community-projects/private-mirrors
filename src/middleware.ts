b62b93188c525b8013bea75c63e261120f2ac119
Jacob Sutter <sutterj@github.com>
2024-05-02 17:02:14 -0400

    fix: build errors
    - Errors were caused by probot middleware being in the app router api
    - Moved webhooks back to pages router and disabled auth middleware for webhooks

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,3 +6,15 @@ export default withAuth({
     error: '/auth/error',
   },
 })
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     */
+    '/((?!api|static|favicon.ico).*)',
+  ],
+}
