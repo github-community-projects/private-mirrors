339cc38890a84ffb7224ae9af858a62d8134385a
Jacob Sutter <sutterj@github.com>
2024-05-16 18:05:33 -0400

    fix: wrap session in use effect

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -4,14 +4,17 @@ import { Box } from '@primer/react'
 import Login from 'app/components/Login'
 import { useSession } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 
 export default function LoginPage() {
   const session = useSession()
   const router = useRouter()
 
-  if (session.data?.user) {
-    router.push('/')
-  }
+  useEffect(() => {
+    if (session.data?.user) {
+      router.push('/')
+    }
+  }, [session, router])
 
   return <Box>{!session.data?.user && <Login />}</Box>
 }
