16bc662d9b993df7121419df67fa0ae751bcbaf9
Jacob Sutter <sutterj@github.com>
2024-05-17 14:49:24 -0400

    fix: move signout on token expiration

--- src/app/[organizationId]/layout.tsx
+++ src/app/[organizationId]/layout.tsx
@@ -2,26 +2,10 @@
 
 import { Box } from '@primer/react'
 import { OrgHeader } from 'app/components/header/OrgHeader'
-import { signOut, useSession } from 'next-auth/react'
-import { useEffect } from 'react'
 import { useOrgData } from 'utils/organization'
 
 const DashLayout = ({ children }: { children: React.ReactNode }) => {
-  const session = useSession()
   const orgData = useOrgData()
-
-  // sign user out if session is expired
-  useEffect(() => {
-    if (!session) {
-      signOut()
-    }
-
-    if (session.data) {
-      if (new Date(session.data.expires) < new Date()) {
-        signOut()
-      }
-    }
-  }, [session])
 
   return (
     <Box sx={{ margin: '10px 90px' }}>
--- src/utils/trpc-provider.tsx
+++ src/utils/trpc-provider.tsx
@@ -2,10 +2,11 @@
 
 import { QueryClientProvider } from '@tanstack/react-query'
 import { getFetch, httpBatchLink, loggerLink } from '@trpc/client'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import superjson from 'superjson'
 import queryClient from './query-client'
 import { trpc } from './trpc'
+import { signOut, useSession } from 'next-auth/react'
 
 export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
   const url = process.env.NEXT_PUBLIC_VERCEL_URL
@@ -32,6 +33,20 @@ export const TrpcProvider = ({ children }: { children: React.ReactNode }) => {
       transformer: superjson,
     }),
   )
+
+  const session = useSession()
+
+  // sign user out if session is expired
+  useEffect(() => {
+    if (
+      !session ||
+      !session.data ||
+      new Date(session.data.expires) < new Date()
+    ) {
+      signOut()
+    }
+  }, [session])
+
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
