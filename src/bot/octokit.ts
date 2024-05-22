90330b2fe8790b515fa5ba2328036848957c887b
Andrew Henry <ajhenry@github.com>
2024-05-22 12:23:09 -0400

    feat: add func to generate pkcs8 pem formats

--- src/bot/octokit.ts
+++ src/bot/octokit.ts
@@ -1,4 +1,5 @@
 import { createAppAuth } from '@octokit/auth-app'
+import { generatePKCS8Key } from 'utils/pem'
 import { logger } from '../utils/logger'
 import { Octokit } from './rest'
 
@@ -7,9 +8,9 @@ const appOctokitLogger = logger.getSubLogger({ name: 'app-octokit' })
 
 // This is a bug with the way the private key is stored in the docker env
 // See https://github.com/moby/moby/issues/46773
-let privateKey = process.env.CONVERTED_PRIVATE_KEY?.includes('\\n')
-  ? process.env.CONVERTED_PRIVATE_KEY.replace(/\\n/g, '\n')
-  : process.env.CONVERTED_PRIVATE_KEY!
+let privateKey = process.env.PRIVATE_KEY?.includes('\\n')
+  ? process.env.PRIVATE_KEY.replace(/\\n/g, '\n')
+  : process.env.PRIVATE_KEY!
 
 /**
  * Generates an app access token for the app or an installation (if installationId is provided)
@@ -17,10 +18,12 @@ let privateKey = process.env.CONVERTED_PRIVATE_KEY?.includes('\\n')
  * @returns An access token for the app or installation
  */
 export const generateAppAccessToken = async (installationId?: string) => {
+  const convertedKey = generatePKCS8Key(privateKey)
+
   if (installationId) {
     const auth = createAppAuth({
       appId: process.env.APP_ID!,
-      privateKey,
+      privateKey: convertedKey,
       installationId: installationId,
     })
 
@@ -50,11 +53,13 @@ export const generateAppAccessToken = async (installationId?: string) => {
  * @returns Octokit authorized as the app
  */
 export const appOctokit = () => {
+  const convertedKey = generatePKCS8Key(privateKey)
+
   return new Octokit({
     authStrategy: createAppAuth,
     auth: {
       appId: process.env.APP_ID!,
-      privateKey,
+      privateKey: convertedKey,
       clientId: process.env.CLIENT_ID!,
       clientSecret: process.env.CLIENT_SECRET!,
     },
@@ -68,11 +73,13 @@ export const appOctokit = () => {
  * @returns Octokit authorized as the installation
  */
 export const installationOctokit = (installationId: string) => {
+  const convertedKey = generatePKCS8Key(privateKey)
+
   return new Octokit({
     authStrategy: createAppAuth,
     auth: {
       appId: process.env.APP_ID!,
-      privateKey,
+      privateKey: convertedKey,
       installationId: installationId,
     },
     log: appOctokitLogger,
