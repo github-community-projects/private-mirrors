7165caa55fd3fb60094206d3c46b38ccd37afc82
Andrew Henry <ajhenry@github.com>
2024-06-11 14:48:16 -0400

    fix: remove unneeded try/catch

--- src/utils/trpc-middleware.ts
+++ src/utils/trpc-middleware.ts
@@ -1,4 +1,3 @@
-import { TRPCError } from '@trpc/server'
 import { checkGitHubAppInstallationAuth, checkGitHubAuth } from './auth'
 import { Middleware } from './trpc-server'
 
@@ -6,20 +5,15 @@ export const verifyGitHubAppAuth: Middleware = async (opts) => {
   const { ctx, rawInput } = opts
 
   // Check app authentication
-  try {
-    await checkGitHubAppInstallationAuth(
-      (rawInput as Record<string, string>)?.accessToken,
-      (rawInput as Record<string, string>)?.mirrorOwner,
-      (rawInput as Record<string, string>)?.mirrorName,
-    )
+  await checkGitHubAppInstallationAuth(
+    (rawInput as Record<string, string>)?.accessToken,
+    (rawInput as Record<string, string>)?.mirrorOwner,
+    (rawInput as Record<string, string>)?.mirrorName,
+  )
 
-    return opts.next({
-      ctx,
-    })
-  } catch (error) {
-    console.error('Error checking github app installation auth', error)
-    throw new TRPCError({ code: 'UNAUTHORIZED' })
-  }
+  return opts.next({
+    ctx,
+  })
 }
 
 export const verifyAuth: Middleware = async (opts) => {
