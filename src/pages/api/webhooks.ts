dc021f525b0d615015e1c6738c7304690036c0ea
Mio Diaz <themio@themioshow.com>
2026-06-18 22:12:53 -0400

    Update webhooks.ts
    Co-authored-by: Riley Kohler <code@rileykohler.com>
    Signed-off-by: Mio Diaz <themio@themioshow.com>

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -22,4 +22,9 @@ export default await createNodeMiddleware(app, {
     },
   }),
   webhooksPath: '/api/webhooks',
+// Probot v14 requires a pino logger so custom logging has been removed
+// In the future it is worth considering replacing tslog with pino entirely
+export default await createNodeMiddleware(app, {
+  probot: createProbot(),
+  webhooksPath: '/api/webhooks',
 })
